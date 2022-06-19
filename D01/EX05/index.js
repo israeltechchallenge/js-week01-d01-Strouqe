aac46c1f150d22af8e2dfdf77f8f2d653cdf5fd5
Strouqe <a.chervonenko@gmail.com>
2022-06-19 18:36:15 +0300

    my first commit

--- D01/EX05/index.js
+++ D01/EX05/index.js
@@ -1,5 +1,18 @@
-function moduloEx5() {
+function moduloEx5(number1,number2) {
   //your code
+  let string = ``
+  if (number1 > number2) {
+    string =`The number ${number1} is bigger than ${number2}`;
+
+    if(number1 % number2 !== 0) {
+      string = string + `. But the modulo of ${number1} % ${number2} is ${number1 % number2}`;
+    } 
+  } else if(number1 === number2) {
+       string = `The number ${number1} is equal to ${number2}`;
+    } else {
+       string = `The number ${number1} is less than ${number2}`;
+    }
+    console.log(string)
 }
 
 // Do not remove or change this line, or the tests won't work
